206685f2ccbc446f34112d28a08222316b014c1a
Frank S. Filz <ffilzlnx@mindspring.com>
2024-07-19 16:47:23 -0700

    STATE: fix a state_t refcount leak and a couple minor cleanups
    nfs4_Check_Stateid returns a state_t when it returns NFS4ERR_REPLAY.
    
    In nfs4_op_lock, the reference on that state_t is not released in the
    case of the replay error.
    
    Note that all other callers to nfs4_Check_Stateid handle releasing the
    state_t rererence on replay correctly.
    
    Also fix a few logging bits identified while auditing open owner
    reference counting.
    
    Change-Id: I7611d6cef839846c6f8f0306c7e046cd2228cd2b
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/Protocols/NFS/nfs4_op_lock.c
+++ src/Protocols/NFS/nfs4_op_lock.c
@@ -202,7 +202,7 @@ enum nfs_req_result nfs4_op_lock(struct nfs_argop4 *op,
 			res_LOCK4->status = nfs_status;
 			LogDebug(COMPONENT_NFS_V4_LOCK,
 				 "LOCK failed nfs4_Check_Stateid for open owner");
-			return NFS_REQ_ERROR;
+			goto out2;
 		}
 
 		open_owner = get_state_owner_ref(state_open);
@@ -315,7 +315,7 @@ enum nfs_req_result nfs4_op_lock(struct nfs_argop4 *op,
 			res_LOCK4->status = nfs_status;
 			LogDebug(COMPONENT_NFS_V4_LOCK,
 				 "LOCK failed nfs4_Check_Stateid for existing lock owner");
-			return NFS_REQ_ERROR;
+			goto out2;
 		}
 
 		/* Check if lock state belongs to same export */
